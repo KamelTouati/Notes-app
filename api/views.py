c73100d936d321775ef45b0925b5211e7b4dc434
KamelTouati <kameltouati19.work@gmail.com>
2023-03-16 23:31:17 +0100

    add getNotes, getNote views

--- api/views.py
+++ api/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+# from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from .models import Note
+from .serializers import NoteSerializer
 # Create your views here.
 
+@api_view(['GET'])
 def getRoutes(request):
     routes = [
         {
@@ -35,4 +41,16 @@ def getRoutes(request):
             'description': 'Deletes and exiting note'
         },
     ]
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
+
+@api_view(['GET'])
+def getNotes(request):
+    notes = Note.objects.all()
+    notes_ser = NoteSerializer(notes, many=True)
+    return Response(notes_ser.data)
+
+@api_view(['GET'])
+def getNote(request, pk):
+    note = Note.objects.get(id=pk)
+    note_ser = NoteSerializer(note, many=False)
+    return Response(note_ser.data)
